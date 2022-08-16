27b0c670a355c92b011670368e4d138d3b86139e
Thomas Eng <tomeng70@gmail.com>
2022-08-16 13:55:51 -0400

    modified display so it looks good on mac

--- main.py
+++ main.py
@@ -13,45 +13,56 @@ start_time = 0
 current_time = 0
 elapsed_time = 0
 
+
 def click_start():
     global current_state
     print("start clicked")
     current_state = STARTED
+
 
 def click_stop():
     global current_state
     print("stop clicked")
     current_state = STOPPED
 
+
 def click_reset():
     global current_state
     print("reset clicked")
     current_state = RESET
+
 
 def click_quit():
     global current_state
     print("quit clicked")
     window.destroy()
 
+
 window = Tk()
 width_value = window.winfo_screenwidth
 height_value = window.winfo_screenheight
 window.geometry("800x600")
 
-button_start = Button(window, text="START", command=click_start, font=("Comic Sans", 30))
-button_start.place(relx = 0.25, rely = 0.94)
+button_start = Button(window, text="START",
+                      command=click_start, font=("Comic Sans", 30))
+button_start.place(relx=0.25, rely=0.9)
 
-button_stop = Button(window, text="STOP ", command=click_stop, font=("Comic Sans", 30))
-button_stop.place(relx = 0.4, rely = 0.94)
+button_stop = Button(window, text="STOP ",
+                     command=click_stop, font=("Comic Sans", 30))
+button_stop.place(relx=0.4, rely=0.9)
 
-button_reset = Button(window, text = "RESET", command=click_reset, font=("Comic Sans", 30))
-button_reset.place(relx = 0.55, rely = 0.94)
+button_reset = Button(window, text="RESET",
+                      command=click_reset, font=("Comic Sans", 30))
+button_reset.place(relx=0.55, rely=0.9)
 
-button_quit = Button(window, text = "QUIT", command=click_quit, font=("Comic Sans", 30))
-button_quit.place(relx = 0.70, rely = 0.94)
+button_quit = Button(window, text="QUIT",
+                     command=click_quit, font=("Comic Sans", 30))
+button_quit.place(relx=0.70, rely=0.9)
 
-# This function is used to 
+# This function is used to
 # display time on the label
+
+
 def update_time():
     global current_state
     global start_time
@@ -67,25 +78,26 @@ def update_time():
     elif (current_state == RUNNING):
         current_time = time.time_ns()
         elapsed_time = current_time - start_time
-        s = format(round(elapsed_time / 1000000000, 3), '010.3f')
-        lbl.config(text = s)
-        
+        s = format(round(elapsed_time / 1000000000, 3), '07.3f')
+        lbl.config(text=s)
+
     elif (current_state == RESET):
         current_state = STOPPED
-        s = format(0, '010.3f')
+        s = format(0, '07.3f')
         print(s)
-        lbl.config(text = s)
+        lbl.config(text=s)
     lbl.after(1, update_time)
-    
-  
+
+
 # Styling the label widget so that clock
 # will look more attractive
-lbl = Label(window, font = ('calibri', 240, 'bold'), background = 'purple', foreground = 'white')
-  
+lbl = Label(window, font=('courier', 310, 'bold'),
+            background='purple', foreground='white')
+
 # Placing clock at the centre
 # of the tkinter window
 #lbl.pack(anchor = 'center')
-lbl.place(relx=0.05, rely=0.1, relheight = 0.8, relwidth=0.9)
+lbl.place(relx=0.05, rely=0.1, relheight=0.6, relwidth=0.9)
 update_time()
 
 
